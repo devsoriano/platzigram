08202abecd0e0ffb0f79f1645c338d1c8d7d9761
Rafael Soriano Ramírez <rafaelsorianoramirez@iMac-de-Rafael.local>
2020-09-05 13:39:25 -0500

    proceso de migración de base de datos

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,5 +1,7 @@
 """Platzigram URL's module"""
 
+# Django
+from django.contrib import admin
 from django.urls import path
 
 from platzigram import views as local_views
@@ -7,6 +9,9 @@ from platzigram import views as local_views
 from posts import views as posts_views
 
 urlpatterns = [
+
+    path('admin/', admin.site.urls),
+
     path('hello-world/', local_views.hello_world),
     path('hi/', local_views.hi),
     path('sorted/', local_views.sorted),
